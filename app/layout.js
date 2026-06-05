e16d9ddcce8cd3a73027e0ffa076c860daeb253a
adimigaker <59336725+adimigaker@users.noreply.github.com>
2026-06-05 09:40:56 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import '../styles/style.css';
-import '../styles/player.css';
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import '../styles/style.css'
+import '../styles/player.css'
 
 export default function RootLayout({ children }) {
   return (
@@ -11,7 +11,25 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Pirate Studio 21 - Streaming Film & Series</title>
         <meta name="description" content="Nonton streaming film dan series subtitle Indonesia gratis." />
-        <meta name="keywords" content="ps21, nonton film, streaming film, anime streaming, movie download" />
+        
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
         <Navbar />
@@ -19,5 +37,5 @@ export default function RootLayout({ children }) {
         <Footer />
       </body>
     </html>
-  );
+  )
 }
