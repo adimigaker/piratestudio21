08d7454f54be70a66e386cb71a05e3bed229107f
adimigaker <59336725+adimigaker@users.noreply.github.com>
2026-06-08 12:22:20 +0700

    Update route.js

--- app/api/films/route.js
+++ app/api/films/route.js
@@ -5,25 +5,23 @@ export async function GET(request) {
   const offset = parseInt(searchParams.get('offset') || '0')
   const limit = parseInt(searchParams.get('limit') || '10')
   const genre = searchParams.get('genre')
-  const sort = searchParams.get('sort') || 'update' // 'update' atau 'year'
+  const sort = searchParams.get('sort') || 'update'
   
   let query = supabase
     .from('PirateStudio21_DB')
     .select('*')
   
-  // Filter by genre
   if (genre) {
     query = query.ilike('genre', `%${genre}%`)
   }
   
-  // Sort berdasarkan pilihan
+  // Sorting berdasarkan pilihan
   if (sort === 'update') {
     query = query.order('updated_at', { ascending: false, nullsFirst: false })
   } else {
     query = query.order('year', { ascending: false })
   }
   
-  // Tambahkan pagination
   if (!genre) {
     query = query.range(offset, offset + limit - 1)
   }
